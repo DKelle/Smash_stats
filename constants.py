1fb17f2c3a046726b59823dbab9b3b0db9457657
Dallas Kelle <dallas.kelle@gmail.com>
2017-11-24 12:47:30 -0600

    Adding a file to hold all constants

---
+++ constants.py
@@ -0,0 +1,14 @@
+TAGS_TO_COALESCE = [['christmas mike', 'thanksgiving mike'],
+        ['megafox', 'su | megafox'],
+        ['hakii', 'su l hakii', 'su | hakii', 'su redriot i hakii', 'hih | hakii', 'su | sleepyhakii', 'su|hakii', 'su | hakii $', 'su  redriot i hakii', 'hoh | hakii', 'su| hakii'],
+        ['lucy', 'ttn | lucy'],
+        ['sassy', 'atx | sassy'],
+        ['crump', 'donald crump', 'captain crump', 'abc | crump'],
+        ['dragonite', 'su dragonite', 'su | dragonite', 'tpwn | dragonite', 'tpwn | dragonite_pr', 'tpwn| dragonite (gnw)', 'atx hoh | dragonite', 'dragonite_pr', 'hoh | dragonite', 'mega dragonite']]
+
+
+AUSTIN_URLS = ['https://challonge.com/NP9ATX###', 'https://austinsmash4.challonge.com/atx###', 'http://challonge.com/heatwave###']
+SMASHBREWS_RULS = ['https://challonge.com/Smashbrews###']
+COLORADO_SINGLES_URLS = ['http://smashco.challonge.com/CSUWW###WUS', 'http://smascho.challonge.com/FCWUA###', 'http://smascho.challonge.com/FCWUIB###']
+COLORADO_DOUBLES_URLS = ['http://smashco.challonge.com/CSUWW###WUD', 'http://smashco.challonge.com/FCWUDC###']
+COLORADO_URLS = COLORADO_SINGLES_URLS + COLORADO_DOUBLES_URLS
